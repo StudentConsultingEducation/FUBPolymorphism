755b3f77a134168194357462c2661f879be33c14
scKeijser <tobias.keijser@studentconsulting.com>
2015-12-03 15:43:21 +0100

    FullyCorrectVersion

---
+++ PolymorphismCorrect/PolymorphismCorrect/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismCorrect
+{
+    class Program
+    {
+        //For assignment 1-6 see UserErrors.cs
+        static void Main(string[] args)
+        {
+            #region Assignment 7,10
+            List<UserErrors> UEList = new List<UserErrors> { new NumericInputError(), new TextInputError(), new OutOfBoundsError(), new SpecialCharacterError(), new WhitespaceError() };
+            #endregion
+            #region Assignment 8,10
+            foreach (UserErrors UE in UEList)
+            {
+                Console.WriteLine(UE.UEMessage());
+            }
+            Console.ReadKey();
+            #endregion
+            #region Assignment11&12
+            //Polymorphism is important to create more modular, easily understood and Object Oriented Programs.
+            //Polymorphism allows you to use less code for more functionality. And requires you to structure the code modularly.
+            //If you change any of the errors listed above for another error it will work just as fine.
+            #endregion
+        }
+    }
+}
---
+++ PolymorphismCorrect/PolymorphismCorrect/UserErrors.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismCorrect
+{
+    #region Assignment1
+    public abstract class UserErrors
+    {
+        #region Assignment2
+        public abstract string UEMessage();
+        #endregion
+    }
+    #endregion
+    #region Assignment3
+    public class NumericInputError : UserErrors
+    {
+        #region Assignment4
+        public override string UEMessage()
+        {
+            return  "You tried to use a numeric input in a text only field. This fired an error!";
+        }
+        #endregion
+    }
+    #endregion
+    #region Assignment5
+    public class TextInputError : UserErrors
+    {
+        #region Assignment6
+        public override string UEMessage()
+        {
+            return "You tried to use a text input in a numeric only field. This fired an error!";
+        }
+        #endregion
+    }
+    #endregion
+    #region Assignment9
+    public class OutOfBoundsError : UserErrors
+    {
+
+        public override string UEMessage()
+        {
+            return "You tried to enter an input out of the given bounds. This fired an error!";
+        }
+
+    }
+    public class WhitespaceError : UserErrors
+    {
+
+        public override string UEMessage()
+        {
+            return "You started your input with whitespace, this is now allowed!";
+        }
+
+    }    public class SpecialCharacterError : UserErrors
+    {
+
+        public override string UEMessage()
+        {
+            return "You tried to use a Special Character that isn't allowed in your input. This fired an Error!";
+        }
+
+    }
+    #endregion
+
+
+}
